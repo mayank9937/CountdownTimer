72448997c694a414093934cf425a31b0ad136c7b
mayank9937 <100352011+mayank9937@users.noreply.github.com>
2022-06-26 13:47:36 +0530

    Timer Code Updated

--- countdownTimer.js
+++ countdownTimer.js
@@ -1,61 +1,105 @@
 window.createCountDownTimer = (StartDateTime,EndDateTime) => {
+
+    function addZero(number) {
+        return (number.toString().length == 1) ? `0${number}` : number;
+    }
   
     if(StartDateTime == undefined && EndDateTime == undefined){
       console.warn('Start Date and End Date Is required and Also End Date is Must be Larger Than Start Date...');
       return { isFinished:true };
     }
     var month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var customStartDate = new Date(StartDateTime),
-        customEndTime = new Date(EndDateTime);
+    var customStartDateTime = new Date(StartDateTime),
+        customEndDateTime = new Date(EndDateTime);
     
-    var startSecond = customStartDate.getSeconds().toString(),
-        startMinute = customStartDate.getMinutes().toString();
-    if(startSecond.length == 1){startSecond = '0'+startSecond;}
-    if(startMinute.length == 1){startMinute = '0'+startMinute;}
+    var startSecond = addZero(customStartDateTime.getSeconds().toString()),
+        startMinute = addZero(customStartDateTime.getMinutes().toString()),
+        endSecond = addZero(customEndDateTime.getSeconds().toString()),
+        endMinute = addZero(customEndDateTime.getMinutes().toString());
+
+    var startDate = new Date(`${customStartDateTime.getDate()} ${month[customStartDateTime.getMonth()]} ${customStartDateTime.getFullYear()} ${customStartDateTime.getHours() +':'+ startMinute +':'+ startSecond}`),
+        endDateTime = new Date(`${customEndDateTime.getDate()} ${month[customEndDateTime.getMonth()]} ${customEndDateTime.getFullYear()} ${customEndDateTime.getHours() +':'+ endMinute +':'+ endSecond}`);
   
-    var endSecond = customEndTime.getSeconds().toString(),
-        endMinute = customEndTime.getMinutes().toString();
-    if(endSecond.length == 1){endSecond = '0'+endSecond;}
-    if(endMinute.length == 1){endMinute = '0'+endMinute;}
+    let currentTime = new Date().getTime(),
+        distance = endDateTime.getTime() - currentTime,
+        days = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0;
   
-    var startDate = new Date(`${customStartDate.getDate()} ${month[customStartDate.getMonth()]} ${customStartDate.getFullYear()} ${customStartDate.getHours() +':'+ startMinute +':'+ startSecond}`),
-        endTime = new Date(`${customEndTime.getDate()} ${month[customEndTime.getMonth()]} ${customEndTime.getFullYear()} ${customEndTime.getHours() +':'+ endMinute +':'+ endSecond}`);
-  
-    var currentTime = new Date().getTime();
-    var distance = endTime.getTime() - currentTime
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    var isFinished = false,
+    let isFinished = false,
         timeStart = (currentTime >= startDate.getTime());
-  
-    if(currentTime > endTime){
+
+    let w_distance = startDate.getTime() - currentTime,
+        w_days = 0,
+        w_hours = 0,
+        w_minutes = 0,
+        w_seconds = 0,
+        isInWaitingMode = false;
+
+    if(timeStart == false){
+        isInWaitingMode = true;
+        w_days = addZero(Math.floor(w_distance / (1000 * 60 * 60 * 24)));
+        w_hours = addZero(Math.floor((w_distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        w_minutes = addZero(Math.floor((w_distance % (1000 * 60 * 60)) / (1000 * 60)));
+        w_seconds = addZero(Math.floor((w_distance % (1000 * 60)) / 1000));
+    }
+
+
+    if(timeStart == true){
+        days = addZero(Math.floor(distance / (1000 * 60 * 60 * 24)));
+        hours = addZero(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        minutes = addZero(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+        seconds = addZero(Math.floor((distance % (1000 * 60)) / 1000));
+    }
+
+    if(currentTime > endDateTime){
       isFinished =  true;
     }
-    var data = {days:days,hours:hours,minutes:minutes,seconds:seconds,isFinished:isFinished,timeStart:timeStart};
+
+    var data = {
+        days:days,
+        hours:hours,
+        minutes:minutes,
+        seconds:seconds,
+        isFinished:isFinished,
+        timeStart:timeStart,
+        isInWaitingMode : isInWaitingMode,
+        waitingTime : {
+            days : w_days,
+            hours : w_hours,
+            minutes : w_minutes,
+            seconds : w_seconds
+        }
+    };
     return data;
   }
 
 //   For Calling Function
-window.createCountDownTimer(`24 Feb 2022 7:00:00 pm`,`24 Feb 2022 7:30:00 pm`);
+// window.createCountDownTimer(`24 Feb 2022 7:00:00 pm`,`24 Feb 2022 7:30:00 pm`);
 
 // You can call this function in thiese format alos.
-window.createCountDownTimer(`24 Feb 2022 10:00:00`,`24 Feb 2022 13:30:00`);
-window.createCountDownTimer(`24-Feb-2022 10:00:00`,`24-Feb-2022 13:30:00`);
-window.createCountDownTimer(`24/Feb/2022 10:00:00`,`24/Feb/2022 13:30:00`);
+// window.createCountDownTimer(`24 Feb 2022 10:00:00`,`24 Feb 2022 13:30:00`);
+// window.createCountDownTimer(`24-Feb-2022 10:00:00`,`24-Feb-2022 13:30:00`);
+// window.createCountDownTimer(`24/Feb/2022 10:00:00`,`24/Feb/2022 13:30:00`);
 
 // This Function will return data in this format.
 /*
   window.createCountDownTimer(`24/Feb/2022 10:00:00`,`24/Feb/2022 20:30:00`);
   Return Data : 
     {
-    "days": 0,
-    "hours": 2,
-    "minutes": 55,
-    "seconds": 1,
-    "isFinished": false,
-    "timeStart": true
+        "days": "02",
+        "hours": "00",
+        "minutes": 44,
+        "seconds": 54,
+        "isFinished": false,
+        "timeStart": true,
+        "isInWaitingMode": false,
+        "waitingTime": {
+            "days": 0,
+            "hours": 0,
+            "minutes": 0,
+            "seconds": 0
+        }
     }
 */
